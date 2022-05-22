f13170256d09c6fd835e17148d15adb43bdbd9d5
Dank Shiku <96992431+Dankshiku@users.noreply.github.com>
2022-05-22 20:30:36 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+##   Shikharsingh  : 	Denial of service attack tool
+##   Author 	   : 	Dank Shiku 
+##   Version 	   : 	1.0
+##   Github 	   : 	https://github.com/Dankshiku
+
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+pkg update
+pkg upgrade
+pkg install python
+pkg install filget
+clear
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+sleep 2
+clear
