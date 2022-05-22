5fe9938b9c1a6b7e194bca1d57bb355d88272270
Dank Shiku <96992431+Dankshiku@users.noreply.github.com>
2022-05-22 21:20:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##   Shikharsingh  : 	Denial of service attack tool
+##   Blackbird   : 	Denial of service attack tool
 ##   Author 	   : 	Dank Shiku 
 ##   Version 	   : 	1.0
 ##   Github 	   : 	https://github.com/Dankshiku
@@ -18,4 +18,4 @@ echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Suc
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
 sleep 4
 clear
-python shikharsingh.py
+python Blackbird.py
