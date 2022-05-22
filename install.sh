9c58c614f646e189060bf3fe3c44284d6be63e14
Dank Shiku <96992431+Dankshiku@users.noreply.github.com>
2022-05-22 20:33:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,18 @@
 ##   Author 	   : 	Dank Shiku 
 ##   Version 	   : 	1.0
 ##   Github 	   : 	https://github.com/Dankshiku
-
+clear
+sleep 2
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
 echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
 echo ""
-pkg update
-pkg upgrade
-pkg install python
+pkg install python -y
 pkg install figlet
 clear
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
-sleep 2
+sleep 4
 clear
+python shikharsingh.py
